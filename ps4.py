2516122300cf2f6936e27e543d4614a7eba2ab2b
mgruben <GrubenM@gmail.com>
2016-05-29 14:22:45 -0500

    Restructure simulationDelayedTreatment to more closely resemble ps3.py
    PS4 calls for the use of histograms and multiple plots, but also for disregarding much of the data which
    is generated by calls to ps3.py.  Accordingly, a rewrite of the function is required, but is inappropriate
    in the ps3.py file, and so must be done here.

--- ps4.py
+++ ps4.py
@@ -8,7 +8,8 @@ from ps3b import *
 #
 # PROBLEM 1
 #        
-def simulationDelayedTreatment(numTrials, delayList):
+def simulationDelayedTreatment(numViruses, maxPop, maxBirthProb, clearProb, resistances,
+                               mutProb, numTrials, delay=150):
     """
     Runs simulations and make histograms for problem 1.
 
@@ -22,13 +23,70 @@ def simulationDelayedTreatment(numTrials, delayList):
     numTrials: number of simulation runs to execute (an integer)
     """
     
+    assert type(numViruses) == int, "numViruses must be an integer"
+    assert numViruses > 0, "numViruses must be positive"
+    assert type(maxPop) == int, "maxPop must be an integer"
+    assert maxPop > 0, "maxPop must be positive"
+    assert 0 <= maxBirthProb <= 1, "maxBirthProb must be between 0 and 1"
+    assert 0 <= clearProb <= 1, "clearProb must be between 0 and 1"
+    assert type(numTrials) == int, "numTrials must be an integer"
+    assert type(resistances) == dict, "resistances must be a dictionary"
+    assert 0 <= mutProb <= 1, "mutProb must be positive"
+    assert numTrials > 0, "numTrials must be positive"
+    assert numTrials <= 100, "numTrials cannot exceed 100"
+    
+    trialResults = []
+    trialAverages = []
+    resistTrialResults = []
+    resistTrialAverages = []
+    virusMaster = []
+    
+    for i in range(numViruses):
+        virusMaster.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
+    
+    for i in range(numTrials):
+        viruses = virusMaster[:]      
+        stepResults = []
+        resistStepResults = []
+        thisPatient = TreatedPatient(viruses, maxPop)
+        for j in range(delay):
+            thisPatient.update()
+            stepResults.append(float(thisPatient.getTotalPop()))
+            resistStepResults.append(float(thisPatient.getResistPop(['guttagonol'])))
+            
+        thisPatient.addPrescription('guttagonol')
+        for j in range(150):
+            thisPatient.update()
+            stepResults.append(float(thisPatient.getTotalPop()))
+            resistStepResults.append(float(thisPatient.getResistPop(['guttagonol'])))
+        trialResults.append(stepResults[:])
+        resistTrialResults.append(resistStepResults[:])
+        
+    for i in range(delay + 150):
+        trialAverages.append(0)
+        resistTrialAverages.append(0)
+        for trial in trialResults:
+            trialAverages[i] += trial[i]
+        for trial in resistTrialResults:
+            resistTrialAverages[i] += trial[i]
+        trialAverages[i] = trialAverages[i] / numTrials
+        resistTrialAverages[i] = resistTrialAverages[i] / numTrials
+    pylab.plot(trialAverages, "ro", label = "Total Virus Population")
+    pylab.plot(resistTrialAverages, "bo", label = "Drug-resistant Virus Population")
+    
+    pylab.title("Simulation of Virus Population Growth with Drug Treatment ("+str(delay)+" delay)")
+    pylab.xlabel("Number of Elapsed Time Steps")
+    pylab.ylabel("Average Size of Virus Population")
+    pylab.legend()
+    pylab.show()
+
     for delay in delayList:
         simulationWithDrug(100, 1000, 0.1, 0.05, {"guttagonol": False}, 0.005, numTrials, delay)
 
 
 
-
-simulationDelayedTreatment(1, [300, 150, 75, 0])
+for delay in [300, 150, 75, 0]:
+    simulationDelayedTreatment(1, delay)
 
 #
 # PROBLEM 2
